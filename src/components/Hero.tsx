37e74331727e9adbb4008afa075598df5b95b205
NisalWishwajith <wishwajithnisal@gmail.com>
2024-09-03 21:51:05 +0530

    modified:   src/components/Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,14 +3,14 @@ export const Hero = () => {
   return (
     <>
       <div className="relative bg-[url('../../public/img/hero.jpg')] bg-cover bg-center p-0 pb-20 pt-40 md:h-auto h-screen">
-        {/* Gradient Overlay gctg */}
+        {/* Gradient Overlay */}
         <div className="absolute inset-0 bg-gradient-to-b from-black/60 via-black/60 to-black/60"></div>
         
         {/* Content Section */}
         <div className="relative flex flex-col place-items-center justify-center text-center py-20 sm:py-32">
   <div className="w-full max-w-4xl px-4 sm:px-6 lg:px-8">
     <h1 className="text-3xl font-bold text-white sm:text-4xl lg:text-5xl xl:text-6xl leading-tight">
-      INpower Laptop Repairing Centertyty
+      INpower Laptop Repairing Center
     </h1>
     <p className="mt-5 text-lg sm:text-xl lg:text-2xl text-gray-200">
       Expert repairs and upgrades to keep your laptop running like new 
