7313cf4f11e582b1fc809b74c3dfc49458751508
Nisal Wishwajith <156841566+NisalWishwajith@users.noreply.github.com>
2024-12-01 19:31:04 +0530

    Update Benefits.tsx

--- src/components/Benefits.tsx
+++ src/components/Benefits.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React from "react";
 import { Container } from "@/components/Container";
 
+
 interface BenefitsProps {
   imgPos?: "left" | "right";
   data: {
